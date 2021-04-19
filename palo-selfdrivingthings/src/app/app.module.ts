aba9849604a478c37806f7a57fd43ad49472eef8
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2021-04-19 10:35:16 +0200

    punto 2

--- palo-selfdrivingthings/src/app/app.module.ts
+++ palo-selfdrivingthings/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DrivingListComponent } from './driving-list/driving-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrivingListComponent
   ],
   imports: [
     BrowserModule,
---
+++ palo-selfdrivingthings/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Driving } from '../driving.model';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent implements OnInit {
+  @Input() listDriving: Driving[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
